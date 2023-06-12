e5fc34e722360edda1cf18bcef0df4faf74b80fd
Nathalie <nat933@free.fr>
2023-06-12 15:57:34 +0200

    change error

--- pages/activitys/activity-card.js
+++ pages/activitys/activity-card.js
@@ -24,7 +24,7 @@ const ActivityCard = (props) => {
                             viewBox="0 0 24 24"
                             strokeWidth="1.5"
                             stroke="currentColor"
-                            class="w-6 h-6">
+                            className="w-6 h-6">
                             <path
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
